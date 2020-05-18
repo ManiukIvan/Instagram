9ec40b09b184d1e49a87363a0fb18dacbf98fbca
ManiukIvan <manuk-ivan@mail.ru>
2020-05-18 14:04:42 +0300

    change design works photo name full name post amount not working

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import {User} from '../../model/user';
+import {ActivatedRoute} from '@angular/router';
+import {UserService} from '../../services/user.service';
+import {LocalStorageService} from '../../services/local-storage.service';
+import {ImageService} from '../../services/image.service';
+import {error} from 'selenium-webdriver';
+import {log} from 'util';
 
 @Component({
   selector: 'app-user',
@@ -7,20 +13,38 @@ import {User} from '../../model/user';
   styleUrls: ['./user.component.css']
 })
 export class UserComponent implements OnInit {
+  errorMessage: string;
   user: User;
-  constructor() {
-    this.user = {
-      id: 1,
-      username: 'Windstorm',
-      fullName: 'Wind Storm Jr.',
-      bio: '',
-      postsAmount: 12,
-      followersAmount: 10,
-      followingAmount: 123,
-    };
+  constructor(private userService: UserService,
+              private localeStorageService: LocalStorageService,
+              private imageService: ImageService,
+              private route: ActivatedRoute) {
+    this.user = null;
   }
 
   ngOnInit(): void {
+    const userName = this.route.snapshot.paramMap.get('userName');
+    const resp = this.userService.getUser(userName);
+    resp.subscribe((user: User) => {
+      this.user = user;
+      }, (error1) => {
+      this.errorMessage = error1.error.message;
+    });
   }
 
+  onProfilePhotoChanged(event) {
+      const selectedFile = event.target.files[0];
+      if (selectedFile !== null && selectedFile !== undefined) {
+        const resp = this.imageService.changeUserPhoto(selectedFile, this.user.id);
+        resp.subscribe((mess: string) => {
+          this.user.avatarImageURL += '?' + selectedFile.name;
+        }, (error1) => {
+         const message = error1.error;
+         console.log(message);
+        });
+      }
+  }
+  isUserOwnComponent(): boolean {
+    return  this.user.userName === this.localeStorageService.getUserName();
+  }
 }
