0a36c7b3bfe5252e501551bebbc3ea3b74e95ac8
ManiukIvan <manuk-ivan@mail.ru>
2020-05-22 16:56:00 +0300

    pages that show all posts

---
+++ src/app/pages/posts-all-page/posts-all-page.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-posts-all-page',
+  templateUrl: './posts-all-page.component.html',
+})
+export class PostsAllPageComponent implements OnInit {
+  constructor() {
+  }
+  ngOnInit(): void {
+  }
+
+}
