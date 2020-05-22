b210afb3bd228acb6dca4b2d5b5dd5a71f054bb5
ManiukIvan <manuk-ivan@mail.ru>
2020-05-22 17:04:37 +0300

    new page that show posts with hashtag

---
+++ src/app/pages/posts-with-hashtag-page/posts-with-hashtag-page.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-posts-with-hashtag-page',
+  templateUrl: './posts-with-hashtag-page.component.html'
+})
+export class PostsWithHashtagPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
