81fe5e69004377c90664b1b21e598d98b288d050
ManiukIvan <manuk-ivan@mail.ru>
2020-05-22 17:04:58 +0300

    update

---
+++ src/app/pages/post-page/post-page.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-post-page',
+  templateUrl: './post-page.component.html',
+})
+export class PostPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
